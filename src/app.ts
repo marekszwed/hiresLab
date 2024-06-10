b964e86be4603c715b1b55550f0f3367921908a6
Marek <s.marekszwed@gmail.com>
2024-06-10 22:08:13 +0200

    Added typescript code for counter in section 'about-us'

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+const counterBox: HTMLElement = document.querySelector('.counter__box--item')!;
+const counterItems = document.querySelectorAll('.number');
+
+const startCounter = (entry: any) => {
+	console.log(entry[0]);
+
+	if (entry[0].isIntersecting) {
+		counterItems.forEach((counter) => {
+			const update = () => {
+				const finalNumber = counter.getAttribute('data-number');
+				const value: number = Number(counter.textContent);
+				console.log(value);
+
+				const speed = Number(finalNumber) / 30;
+
+				if (value < Number(finalNumber)) {
+					counter.textContent = `${Math.floor(value + speed)}`;
+					setTimeout(update, 20);
+				} else {
+					counter.textContent = finalNumber;
+				}
+			};
+
+			update();
+		});
+	}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	const observer = new IntersectionObserver(startCounter);
+
+	observer.observe(counterBox);
+});
